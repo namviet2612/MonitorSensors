bc57179744c0e7fd2a70e31c21a14a3e4fa4fa3a
Nam Nguyen <namviet2612@gmail.com>
2021-10-08 23:40:51 +0700

    Initial commit

--- MonitorSensors/MonitorSensors.py
+++ MonitorSensors/MonitorSensors.py
@@ -1,3 +1,20 @@
 
-import serial as sr
+from math import radians
+import numpy as np     # installed with matplotlib
+import matplotlib.pyplot as plt
+
+import serial
+
+def serialDataFetching():
+    ser = serial.Serial('COM4', 19323)
+    print(ser.name)
+
+def dataPlot():
+    x = np.arange(0, radians(1800), radians(12))
+    plt.plot(x, np.cos(x), 'b')
+    plt.show()
+
+serialDataFetching()
+
+
 
---
+++ readme.txt
@@ -0,0 +1,8 @@
+Environment:
+- Visual Studio Community
+- Python for development includes in the installation package: Python 3.7 (64 bit)
+
+Prerequiste:
+- matplotlib 3.4.3
+- numpy 1.21.2
+- pyserial 3.5
